d05c052c120ca6fefdebf1fdb8ec7e4e040a35d8
Lulama Lingela <lulamahana@gmail.com>
2025-08-13 23:11:44 +0200

    prac 1b code up
    - fixed-pt updated
    - main method runs benchmarks

--- EEE3096S 2025 Repo/Practical1/Practical_1B/Core/Src/main.c
+++ EEE3096S 2025 Repo/Practical1/Practical_1B/Core/Src/main.c
@@ -130,7 +130,7 @@ int main(void)
 
       // Double benchmark:
       //TODO: Record the start time
-i      start_time = HAL_GetTick();
+      start_time = HAL_GetTick();
       //TODO: Call the Mandelbrot Function and store the output in the checksum variable defined initially
       checksum = calculate_mandelbrot_double(width, height, MAX_ITER);
       //TODO: Record the end time
@@ -229,44 +229,47 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 //TODO: Mandelbroat using variable type integers and fixed point arithmetic
-#define SCALE 1000000 // scaling factor (1.0 in fixed-point)
-uint64_t calculate_mandelbrot_fixed_point_arithmetic(int width, int height, int max_iterations) {
-    uint64_t mandelbrot_sum = 0;
+#define SCALE 4096 // scaling factor
+uint64_t calculate_mandelbrot_fixed_point_arithmetic(int width, int height, int max_iterations){
+  uint64_t mandelbrot_sum = 0;
+    //TODO: Complete the function implementation
+    for(int y = 0; y < height; y++){
+    	for(int x = 0; x < width; x++){
+    		// convert pixel coordinates to fixed point complex plane coordinates
+    		int32_t x0 = ((int64_t)x*SCALE*35/(width*10))-(SCALE*25)/10; // making sure all numbers are integers not floats
+    		int32_t y0 = ((int64_t)y * SCALE * 2/ height)-SCALE;
 
-    for (int y = 0; y < height; y++) {
-        // y0 = (y / height) * 2.0 - 1.0
-        int32_t y0 = ((int64_t)y * (SCALE * 2) / height) - SCALE;
+    		int32_t xi = 0;
+    		int32_t yi = 0;
+    		int i = 0;
 
-        for (int x = 0; x < width; x++) {
-            // x0 = (x / width) * 3.5 - 2.5
-            int32_t x0 = ((int64_t)x * (SCALE * 35 / 10) / width) - (SCALE * 25 / 10);
+    		while (i < max_iterations){
+    			int64_t xi_sq = ((int64_t)xi * xi) / SCALE;
+    			int64_t yi_sq = ((int64_t)yi * yi) / SCALE;
 
-            int32_t xi = 0;
-            int32_t yi = 0;
-            int i = 0;
+    			if(xi_sq + yi_sq <= 4 * SCALE){
+    				int32_t temp = (int32_t)(xi_sq - yi_sq);
 
-            while (i < max_iterations) {
-                int64_t xi_sq = ((int64_t)xi * xi) / SCALE;
-                int64_t yi_sq = ((int64_t)yi * yi) / SCALE;
+    				int64_t xy = ((int64_t)xi * yi) / SCALE;
 
-                if (xi_sq + yi_sq <= 4 * SCALE) {
-                    int32_t temp = (int32_t)(xi_sq - yi_sq + x0);
+    				yi  =(int32_t)(2*xy + y0);
+    				xi = temp + x0;
 
-                    int64_t xy = ((int64_t)xi * yi) / SCALE;
-                    yi = (int32_t)(2 * xy + y0);
-                    xi = temp;
+    				i++;
 
-                    i++;
-                } else {
-                    break;
-                }
-            }
+    			}
 
-            mandelbrot_sum += (uint64_t)i;
-        }
+    			else{
+    				break;
+    			}
+    		}
+
+    		mandelbrot_sum += i;
+
+    	}
     }
-
     return mandelbrot_sum;
+
 }
 
 
